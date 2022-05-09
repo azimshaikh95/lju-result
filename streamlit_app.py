ac3ef3a86dbf7255eb29234ad014f6ef1c48f9c7
MohammedAzim Shaikh <azim.music9@gmail.com>
2022-05-09 20:20:37 +0530

    Updated on 2022-05-09 20:20:37

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ if (sidebarContent == "Semester Exam Report"):
         st.markdown('<body class= "last" >Developed & Managed By: <a href="https://in.linkedin.com/in/mohammedazim-shaikh">MohammedAzim Shaikh</a></body>', unsafe_allow_html=True)
         st.write("...")
         
-col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns([3, 1, 1])
 
 with col1:
     st.header("A cat")
