e3b7c58d8c5f9dab690044127d37a87839a85863
MohammedAzim Shaikh <azim.music9@gmail.com>
2022-05-09 20:40:45 +0530

    Updated on 2022-05-09 20:40:45

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,14 +129,7 @@ template = env.get_template("template.html")
 
 left.write("Fill in the data:")
 form = left.form("template_form")
-student = form.text_input("StudentName")
-studen2t = form.text_input("StudentName33")
-course = form.selectbox(
-    "Choose course",
-    ["Report Generation in Streamlit", "Advanced Cryptography"],
-    index=0,
-)
-grade = form.slider("Grade", 1, 100, 60)
+
 submit = form.form_submit_button("Generate PDF")
 
 if submit:
