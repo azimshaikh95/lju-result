90517e3e6e16ad900df495a2224625f8a7b5d276
MohammedAzim Shaikh <azim.music9@gmail.com>
2022-05-09 20:26:26 +0530

    Updated on 2022-05-09 20:26:26

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,19 +95,19 @@ if (sidebarContent == "Semester Exam Report"):
 col1, col2, col3, col4 = st.columns([3, 1, 1, 1])
 
 with col1:
-    st.header("A cat")
+    st.write("**Subject Code and Name**")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col2:
-    st.header("A dog")
+    st.write("**Theory Grade**")
     st.image("https://static.streamlit.io/examples/dog.jpg")
 
 with col3:
-    st.header("An owl")
+    st.write("**Practical Grade**")
     st.image("https://static.streamlit.io/examples/owl.jpg")
     
 with col4:
-    st.header("An owl")
+    st.write("**Overall Grade**")
     st.image("https://static.streamlit.io/examples/owl.jpg")
 
 
