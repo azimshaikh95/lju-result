9d7880118ee2fd2f63bc000e13c87bd5f05dc808
Azim <azim.music9@gmail.com>
2022-05-12 08:49:41 +0530

    Updated on 2022-05-12 08:49:41

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if (sidebarContent == "Semester Exam Report"):
         st.image('ljulogo.png', use_column_width=True)
         st.markdown("<h1 style='text-align: center'><b>Semester Exam Report</b></h1>", unsafe_allow_html=True)
         st.markdown("<h1 style='text-align: center'><b>LJ Polytehnic</b></h1>", unsafe_allow_html=True)
-        st.write("Last Updated On: " + lastupdated )
+        st.write("<b>Last</b> Updated On: " + lastupdated )
         st.write("#####")
 
     with(login):
