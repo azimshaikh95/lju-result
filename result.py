ac7d11147910c55f88042f6f6b249e9d3f72f88a
Azim <azim.music9@gmail.com>
2022-05-12 12:15:57 +0530

    Updated on 2022-05-12 12:15:57

--- result.py
+++ result.py
@@ -1,39 +1,9 @@
-Set Up Git:
 git config --global user.name "Azim"
-git config --global user.email "azim.music9.com"
+git config --global user.email "azim.music9@gmail.com"
 
 other commands
-
-git clone https://github.com/azimshaikh95/lju-result.git
-git add .
-git commit -m "Updated on `date +'%Y-%m-%d %H:%M:%S'`";
-git push -u origin master
-git remote add origin master https://github.com/azimshaikh95/lju-result.git
-
-
-===============
-cd LJU-Result-App
-
-git status
+git clone (url)
 git add .
 git commit -m "Updated on `date +'%Y-%m-%d %H:%M:%S'`";
 git push -u origin main
-git status
-
-===================
-echo "# lju-result" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/azimshaikh95/lju-result.git
-git push -u origin main
-
-
-
-
-
-…or push an existing repository from the command line
-git remote add origin https://github.com/azimshaikh95/lju-result.git
-git branch -M main
-git push -u origin main
+git remote add origin main git@github.com:azimshaikh95/lju-result.git
