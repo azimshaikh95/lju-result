40c2019cb1fb08b37fe5f28701ef9f0e2e5efe95
Azim <azim.music9@gmail.com>
2022-05-12 08:45:54 +0530

    Updated on 2022-05-12 08:45:54

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,11 +50,11 @@ for i in range(len(df["EnrolmentNo"])):
 
 
 #WebApp -- "LJ University Result"
-# sidebarContent = st.sidebar.radio("Menu", ["Semester Exam Report", "Milestone Leaderboard", "Program Resources"])
+sidebarContent = st.sidebar.radio("Menu", ["Semester Exam Report"])
 
 
 #SER
-# if (sidebarContent == "Semester Exam Report"):
+if (sidebarContent == "Semester Exam Report"):
     with(header):
         st.image('ljulogo.png', use_column_width=True)
         st.markdown("<h1 style='text-align: center'><b>Semester Exam Report</b></h1>", unsafe_allow_html=True)
