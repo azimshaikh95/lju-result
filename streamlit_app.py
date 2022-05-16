bf1790fbb9064fec235e34dac2a4d7b9641d7d71
Azim <azim.music9@gmail.com>
2022-05-16 11:20:30 +0530

    Updated on 2022-05-16 11:20:30

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,7 +143,7 @@ if(textInput != "" and status):
             CurrentBacklog=str(df["CurrentBacklog"][tindex]),
             TotalBacklog=str(df["TotalBacklog"][tindex]),
             DeclarationDate=str(df["DeclarationDate"][tindex]),
-            student_pic=image_file_path_to_base64_string('ljulogo.png'),
+            student_pic=image_file_path_to_base64_string('m060.png'),
             logo_img_string=image_file_path_to_base64_string('ljulogo.png'),
         )
 
