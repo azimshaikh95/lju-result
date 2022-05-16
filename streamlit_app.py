899c5225e3b0116db157e65b7e44bf9da14ecff1
Azim <azim.music9@gmail.com>
2022-05-16 11:50:12 +0530

    Updated on 2022-05-16 11:50:12

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,18 +43,17 @@ df = pd.DataFrame(data)
 
 
 for i in range(len(df["EnrolmentNo"])):
-    # df['EnrolmentNo'][i] = df['EnrolmentNo'][i].lower()
-    df['EnrolmentNo'][i] = df['EnrolmentNo'][i]
+    df['EnrolmentNo'][i] = df['EnrolmentNo'][i].lower()
+    # df['EnrolmentNo'][i] = df['EnrolmentNo'][i]
     
 st.image('ljulogo.png', use_column_width=True)
 st.markdown("<h1 style='text-align: center'><b>Semester Exam Report</b></h1>", unsafe_allow_html=True)
 
 
-# textInput = st.text_input("Enter your Enrolment No").lower()
-textInput = st.text_input("Enter your Enrolment No")
+textInput = st.text_input("Enter your Enrolment No").lower()
+# textInput = st.text_input("Enter your Enrolment No")
 
 #Input Activity
-st.markdown( type(textInput) )
 status = False
 for i in df["EnrolmentNo"]:
     if( i == textInput):
